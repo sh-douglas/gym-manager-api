62b1ee51965840db16d9082c5a742342f9a0553d
Douglas <douglasd3v.contato@gmail.com>
2026-07-11 18:56:59 -0300

    feat: add student update validator

--- src/validators/student-validator.ts
+++ src/validators/student-validator.ts
@@ -20,6 +20,42 @@ const createStudentSchema = z.object({
     }),
 });
 
-type CreateStudentInput = z.infer<typeof createStudentSchema>;
+const updateStudentSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(3, "The name field must contain at least 3 characters.")
+      .optional(),
+    email: z.string().trim().email("The email field must be valid.").optional(),
+    phone: z.string().trim().e164().optional(),
+    birthDate: z
+      .string()
+      .date("The date must be in YYYY-MM-DD format.")
+      .refine((val) => new Date(val) <= new Date(), {
+        message: "The birth date cannot be in the future.",
+      })
+      .optional(),
+  })
+  .refine(
+    (data) => {
+      const fields = Object.keys(data).filter(
+        (key) => data[key as keyof typeof data] !== undefined,
+      );
+      return fields.length > 0;
+    },
+    {
+      message: "At least one field must be provided for updating.",
+      path: [],
+    },
+  );
 
-export { createStudentSchema, type CreateStudentInput };
+type CreateStudentInput = z.infer<typeof createStudentSchema>;
+type UpdateStudentInput = z.infer<typeof updateStudentSchema>;
+
+export {
+  createStudentSchema,
+  type CreateStudentInput,
+  updateStudentSchema,
+  type UpdateStudentInput,
+};
